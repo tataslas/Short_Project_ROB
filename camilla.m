db857bda9ca5012911656a71bfd51ce7d5e61433
tataslas <sandrafh@hotmail.es>
2019-03-30 22:03:41 +0100

    script cilindro, color vertices quitado

--- camilla.m
+++ camilla.m
@@ -5,7 +5,7 @@ load('/usr/local/MATLAB/R2016b/licenses/short_project_1/SP_material/F_V_Human_Bo
 
 
 %HUMAN BODY
-patch('Faces',F,'Vertices',V,'FaceColor','green')
+patch('Faces',F,'Vertices',V,'FaceColor','white')
 axis equal
 %axis ([20 -20 20 -20 20 -20])
 hold on
@@ -18,25 +18,27 @@ C = [0.5];
 % labelling
 zlabel('z');ylabel('y');xlabel('x');
 % drawing the figure
-fill3(X_m,Y_m,Z_m,C,'FaceColor', [.37 .45 .64], 'FaceLighting', 'gouraud');
+fill3(X_m,Y_m,Z_m,C,'FaceColor', [.37 .45 .64], 'FaceLighting', 'gouraud', 'EdgeColor', [.37 .45 .64]);
+material metal;
 
 %PATAS
 [Xp1,Yp1,Zp1] = cylinder(0.3);
 Zp1(2,:) = 5;
-surface(Xp1+5,Yp1+20,Zp1-7,'FaceColor', [.37 .45 .64] );
+surface(Xp1+5,Yp1+20,Zp1-7,'FaceColor', [.37 .45 .64], 'EdgeColor', [.37 .45 .64]);
+
 
 [Xp2,Yp2,Zp2] = cylinder(0.3);
 Zp2(2,:) = 5;
-surface(Xp2-5,Yp2+20,Zp2-7,'FaceColor', [.37 .45 .64] );
+surface(Xp2-5,Yp2+20,Zp2-7,'FaceColor', [.37 .45 .64], 'EdgeColor', [.37 .45 .64] );
 
 [Xp3,Yp3,Zp3] = cylinder(0.3);
 Zp3(2,:) = 5;
-surface(Xp3+5,Yp3+1,Zp3-7,'FaceColor', [.37 .45 .64] );
+surface(Xp3+5,Yp3+1,Zp3-7,'FaceColor', [.37 .45 .64], 'EdgeColor', [.37 .45 .64] );
 
 [Xp4,Yp4,Zp4] = cylinder(0.3);
 Zp4(2,:) = 5;
-surface(Xp4-5,Yp4+1,Zp4-7,'FaceColor', [.37 .45 .64] );
+surface(Xp4-5,Yp4+1,Zp4-7,'FaceColor', [.37 .45 .64], 'EdgeColor', [.37 .45 .64]);
 
 
 
-view();
+view(3);
