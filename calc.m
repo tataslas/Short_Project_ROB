9f5e69c87992a574442727cb7ad0a4d4482df9cc
tataslas <sandrafh@hotmail.es>
2019-04-22 13:56:36 +0200

    Arreglo matriz e intento de puma operando

--- calc.m
+++ calc.m
@@ -65,10 +65,6 @@ tor2Dicom(1) = (tor2Dicom(1) * mmCoord_X) + origen(1)
 tor2Dicom(2) = (tor2Dicom(2) * mmCoord_Y) + origen(2)
 tor2Dicom(3) = (tor2Dicom(3) * mmCoord_Z) + origen(3)
 
-tor2Dicom(1) = origen(1) + (tor2Dicom(1) * mmCoord_X)
-tor2Dicom(2) = origen(2) + (tor2Dicom(2) * mmCoord_Y)
-tor2Dicom(3) = origen(3) + (tor2Dicom(3) * mmCoord_Z)
-
 tor3Dicom(1) = (tor3Dicom(1) * mmCoord_X) + origen(1)
 tor3Dicom(2) = (tor3Dicom(2) * mmCoord_Y) + origen(2)
 tor3Dicom(3) = (tor3Dicom(3) * mmCoord_Z) + origen(3)
