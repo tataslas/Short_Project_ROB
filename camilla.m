051baa3dcedca0969daae40e3998b7179650180c
tataslas <38156502+tataslas@users.noreply.github.com>
2019-04-24 19:08:13 +0200

    Patas mesas, humano bien, y esfera en fiducials

--- camilla.m
+++ camilla.m
@@ -1,5 +1,7 @@
+
 close all
 clear
+
 
 % CARGA CRANI
 [Vc, Fc, Nc, Crani] = stlRead('Crani.stl');
@@ -15,15 +17,14 @@ stlPlot(Vc, Fc, Crani)
 
 %PUMA
 mdl_puma560
-p560.plot(qz)	
+p560.plot(qz,'nojoints')	
 
 %HUMAN BODY
 load('F_V_Human_Body.mat')
 V = V/10;
 V = V*rotz(-pi/2);
 V(:,1) = V(:,1)+2.7;
-
-patch('Faces',F,'Vertices',V,'FaceColor','white')
+stlPlot(V,F)
 axis equal
 hold on
 
@@ -43,25 +44,25 @@ material metal;
 
 %PATAS MESA OPERACIONES
 [Xp1,Yp1,Zp1] = cylinder(0.3);
-Zp1(2,:) = 5;
-
+Zp1(2,:) = 8;
 surface((Xp1)/10 + 0.75,(Yp1)/10 + 0.5,(Zp1-10)/10,'FaceColor', [.37 .45 .64], 'EdgeColor', [.37 .45 .64]);
 
 [Xp2,Yp2,Zp2] = cylinder(0.3);
-Zp2(2,:) = 5;
+Zp2(2,:) = 8;
 surface((Xp2)/10 + 0.75,(Yp2)/10 - 0.5,(Zp2-10)/10,'FaceColor', [.37 .45 .64], 'EdgeColor', [.37 .45 .64] );
 
 [Xp3,Yp3,Zp3] = cylinder(0.3);
-Zp3(2,:) = 5;
+Zp3(2,:) = 8;
 surface((Xp3)/10 + 2.6,(Yp3)/10 + 0.5,(Zp3-10)/10,'FaceColor', [.37 .45 .64], 'EdgeColor', [.37 .45 .64] );
 
 [Xp4,Yp4,Zp4] = cylinder(0.3);
-Zp4(2,:) = 5;
+Zp4(2,:) = 8;
 surface((Xp4)/10 + 2.6,(Yp4)/10 - 0.5,(Zp4-10)/10,'FaceColor', [.37 .45 .64], 'EdgeColor', [.37 .45 .64]);
 
 
 desplz_x = 1.5
 desplz_y = 1.5
+
 
 
 %MESA AUXILIAR
@@ -75,27 +76,30 @@ Z_ma = Z_ma/10
 %C = [0.5];
 % labelling
 zlabel('z');ylabel('y');xlabel('x');
+
 % drawing the figure
 fill3(X_ma,Y_ma,Z_ma,C,'FaceColor', [.37 .45 .64], 'FaceLighting', 'gouraud', 'EdgeColor', [.37 .45 .64]);
 material metal;
 
+
+
 %PATAS MESA AUXILIAR
 [Xpa1,Ypa1,Zpa1] = cylinder(0.3);
-Zpa1(2,:) = 5;
+Zpa1(2,:) = 8;
 surface((Xpa1-12.5)/10+desplz_x,(Ypa1+23.5)/10-desplz_y,(Zpa1-10)/10,'FaceColor', [.37 .45 .64], 'EdgeColor', [.37 .45 .64]);
 
 [Xpa2,Ypa2,Zpa2] = cylinder(0.3);
-Zpa2(2,:) = 5;
+Zpa2(2,:) = 8;
 surface((Xpa2-12.5)/10+desplz_x,(Ypa2+27.5)/10-desplz_y,(Zpa2-10)/10,'FaceColor', [.37 .45 .64], 'EdgeColor', [.37 .45 .64] );
 
 [Xpa3,Ypa3,Zpa3] = cylinder(0.3);
-Zpa3(2,:) = 5;
+Zpa3(2,:) = 8;
 surface((Xpa3-8.5)/10+desplz_x,(Ypa3+23.5)/10-desplz_y,(Zpa3-10)/10,'FaceColor', [.37 .45 .64], 'EdgeColor', [.37 .45 .64] );
 
 [Xpa4,Ypa4,Zpa4] = cylinder(0.3);
-Zpa4(2,:) = 5;
+Zpa4(2,:) = 8;
 surface((Xpa4-8.5)/10+desplz_x,(Ypa4+27.5)/10-desplz_y,(Zpa4-10)/10,'FaceColor', [.37 .45 .64], 'EdgeColor', [.37 .45 .64]);
 
 
-view(3);
 
+view(3);
--- fiducials.m
+++ fiducials.m
@@ -1,9 +1,10 @@
 
 %HACER QUE EL PUMA VAYA A LOS FIDUCIALS
-tor1Dicom = [19*1.4,121.9,210.1]
-tor2Dicom = [48*1.4,186.56,210.1]
-tor3Dicom = [81*1.4,70.37,214.37]
+tor1Dicom = [19*1.4,117.4287,195.4341]
+tor2Dicom = [48*1.4,175.3122,194.7039]
+tor3Dicom = [81*1.4,68.6949,199.5044]
 
+%de mm a coordenadas
 tor1Dicom(1) = (tor1Dicom(1) * mmCoord_X) + origen(1)
 tor1Dicom(2) = -((tor1Dicom(2) * mmCoord_Y) + origen(2))
 tor1Dicom(3) = -((tor1Dicom(3) * mmCoord_Z) + origen(3))
@@ -15,6 +16,23 @@ tor2Dicom(3) = -((tor2Dicom(3) * mmCoord_Z) + origen(3))
 tor3Dicom(1) = (tor3Dicom(1) * mmCoord_X) + origen(1)
 tor3Dicom(2) = -((tor3Dicom(2) * mmCoord_Y) + origen(2))
 tor3Dicom(3) = -((tor3Dicom(3) * mmCoord_Z) + origen(3))
+
+radius=0.005;
+
+[Vs, Fs, Ns, sphere] = stlRead('sphere_ascii.stl');
+esfera.vertices = Vs;
+esfera.faces = Fs;
+stlPlot(Vs,Fs,sphere)
+patch(esfera,'FaceColor',[0.25,0.25,0.25]);
+axis equal
+
+circle1 = sphere(tor1Dicom, radius);
+circle2 = circle(tor2Dicom, radius);
+circle3 = circle(tor3Dicom, radius);
+
+plot3(circle1(1,:), circle1(2,:), circle1(3,:),'r');
+plot3(circle2(1,:), circle2(2,:), circle2(3,:),'r');
+plot3(circle3(1,:), circle3(2,:), circle3(3,:),'r');
 
 Q= p560.ikine6s(tor1Dicom, 'run')
 p560.plot(Q)
